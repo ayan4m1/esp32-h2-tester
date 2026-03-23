41f6d5b31c8fe4eb5fc224a5e22d513528ee91f6
ayan4m1 <andrew@bulletlogic.com>
2026-03-23 19:24:13 -0400

    wip humidity rectangle painter

--- main/main.cpp
+++ main/main.cpp
@@ -64,7 +64,15 @@
 #define WIFI_SSID "qux"
 #define WIFI_PSK "changeme"
 
-#define OWM_API_URL "http://api.openweathermap.org/data/2.5/weather?lat=40.12225&lon=-75.14492&appid=changeme"
+/* #define OWM_API_URL                                          \
+   "http://api.openweathermap.org/data/2.5/"                  \
+   "weather?units=imperial&lat=40.12225&lon=-75.14492&appid=" \
+   "8ef6a11c58197094daec15f0db44ecab" */
+
+#define OWM_API_URL                                          \
+  "http://api.openweathermap.org/data/3.0/"                  \
+  "onecall?units=imperial&lat=40.12225&lon=-75.14492&appid=" \
+  "changeme&exclude=minutely,hourly,alerts"
 
 #define NTP_SERVER "pool.ntp.org"
 
@@ -81,25 +89,25 @@ static wifi_manager wifi;
 // static OpenWeatherMap weather;
 
 static const char* time_format_string = " %02d:%02d";
-static const char* temp_format_string = "%6.1f °F";
+static const char* long_time_format_string = "      %02d:%02d";
 static const char* short_temp_format_string = "%.1f°F";
-static const char* pres_format_string = "%6d mb";
-static const char* hume_format_string = "%6d %%RH";
+static const char* temp_range_format_string = "    %.0f-%.0f°F";
+static const char* temp_format_string = "     %.1f°F";
 static const char* connecting_format_string = "Connecting...";
 static const char* sync_time_format_string = "Sync Time...";
 static const char* fetching_format_string = "Fetching...";
 
 // to hold API response data
 static char weather_desc[40] = {0};
-static int sunrise, sunset, pressure, weather_id, humidity;
-static float temp;
+static int sunrise, sunset, weather_id, humidity;
+static float temp, min_temp, max_temp;
 
 // to hold strings formatted for display
-static char temp_text[15] = {0};
 static char short_temp_text[15] = {0};
-static char pres_text[15] = {0};
+static char temp_range_text[15] = {0};
+static char temp_text[15] = {0};
 static char time_text[15] = {0};
-static char hume_text[15] = {0};
+static char long_time_text[15] = {0};
 
 void lcd_flush_complete(void) {
   // let UIX know the DMA transfer completed
@@ -109,22 +117,18 @@ static void uix_flush(const rect16& bounds, const void* bitmap, void* state) {
   // similar to LVGL
   lcd_flush(bounds.x1, bounds.y1, bounds.x2, bounds.y2, (void*)bitmap);
 }
-#if LCD_COLOR_SPACE == LCD_COLOR_GSC
-#define PIXEL gsc_pixel<LCD_BIT_DEPTH>
-#else
-#define PIXEL rgb_pixel<LCD_BIT_DEPTH>
-#endif
 // the following can be abbreviated as
 // using screen_t = uix::screen<rgb_pixel<16>>;
 // for most configurations
-using screen_t = uix::screen_ex<bitmap<PIXEL>, LCD_X_ALIGN, LCD_Y_ALIGN>;
+using screen_t =
+    uix::screen_ex<bitmap<gsc_pixel<LCD_BIT_DEPTH>>, LCD_X_ALIGN, LCD_Y_ALIGN>;
 
 // native screen color
 using scr_color_t = color<screen_t::pixel_type>;
 // UIX color (rgba32)
 using uix_color_t = color<uix_pixel>;
 
-static tt_font small_text_font(telegrama, 10, font_size_units::px);
+static tt_font small_text_font(telegrama, 12, font_size_units::px);
 static tt_font large_text_font(telegrama, 14, font_size_units::px);
 static tt_font clock_text_font(telegrama, 28, font_size_units::px);
 
@@ -136,29 +140,24 @@ static screen_t loading_screen;
 using label_t = uix::label<screen_t::control_surface_type>;
 using icon_t = uix::image_box<screen_t::control_surface_type>;
 using vlabel_t = uix::vlabel<screen_t::control_surface_type>;
+using rect_t = uix::painter<screen_t::control_surface_type>;
 
 label_t loading_label;
 label_t time_label;
 label_t temp_label;
-label_t hume_label;
-label_t pres_label;
+label_t temp_range_label;
+rect_t hume_rect;
+rect_t hume_border;
 icon_t weather_icon;
 
 typedef struct {
   bool state;
   bool rightAlign;
 } animate_time_label_param_t;
-typedef struct {
-  bool state;
-  label_t* label;
-} animate_small_label_param_t;
 
 static animate_time_label_param_t show_label_left = {true, false};
-static animate_time_label_param_t show_label_right = {true, false};
+static animate_time_label_param_t show_label_right = {true, true};
 static animate_time_label_param_t hide_label = {false, false};
-static animate_small_label_param_t hide_temp = {false, &temp_label};
-static animate_small_label_param_t hide_pres = {false, &pres_label};
-static animate_small_label_param_t hide_hume = {false, &hume_label};
 
 void animate_time_label(void* parameters) {
   auto params = (animate_time_label_param_t*)parameters;
@@ -170,22 +169,9 @@ void animate_time_label(void* parameters) {
   while (state ? x1-- > (rightAlign ? 16 : 0) : x1++ < LCD_WIDTH - 1) {
     bounds.x1 = x1;
     time_label.bounds(bounds);
-    lcd.update();
-    vTaskDelay(pdMS_TO_TICKS(16));
-  }
-}
-
-void animate_small_label(void* parameters) {
-  auto params = (animate_small_label_param_t*)parameters;
-  bool state = params->state;
-  auto bounds = srect16(params->label->bounds());
-  auto initial_y = bounds.y1;
-  uint16_t y1 = bounds.y1;
-
-  while (state ? y1-- > initial_y : y1++ < initial_y + 10) {
-    bounds.y1 = y1;
-    params->label->bounds(bounds);
-    lcd.update();
+    while (lcd.dirty()) {
+      lcd.update();
+    }
     vTaskDelay(pdMS_TO_TICKS(16));
   }
 }
@@ -210,7 +196,7 @@ void fetch_data() {
   http_stream stream(handle);
   json_reader_ex<64> reader(stream);
 
-  enum { J_START = 0, J_MAIN, J_WEATHER, J_SYS };
+  enum { J_START = 0, J_CURRENT, J_CURRENT_WEATHER, J_DAILY, J_DAILY_TEMP };
   int state = J_START;
 
   while (reader.read()) {
@@ -219,56 +205,64 @@ void fetch_data() {
         continue;
       }
 
-      if (!strcmp("weather", reader.value())) {
-        state = J_WEATHER;
-      } else if (!strcmp("sys", reader.value())) {
-        state = J_SYS;
-      } else if (!strcmp("main", reader.value())) {
-        state = J_MAIN;
+      if (!strcmp("current", reader.value())) {
+        state = J_CURRENT;
+      } else if (!strcmp("daily", reader.value())) {
+        state = J_DAILY;
       }
     } else {
       if (reader.node_type() != json_node_type::field) {
         continue;
       }
 
-      if (state == J_WEATHER) {
+      if (state == J_CURRENT_WEATHER) {
         if (!strcmp("id", reader.value()) && reader.read()) {
           weather_id = reader.value_int();
         } else if (!strcmp("description", reader.value()) && reader.read()) {
           strcpy(weather_desc, reader.value());
         }
-      } else if (state == J_SYS) {
+      } else if (state == J_CURRENT) {
         if (!strcmp("sunrise", reader.value()) && reader.read()) {
           sunrise = reader.value_int();
         } else if (!strcmp("sunset", reader.value()) && reader.read()) {
           sunset = reader.value_int();
-        }
-      } else if (state == J_MAIN) {
-        if (!strcmp("temp", reader.value()) && reader.read()) {
+        } else if (!strcmp("temp", reader.value()) && reader.read()) {
           temp = reader.value_real();
-        } else if (!strcmp("pressure", reader.value()) && reader.read()) {
-          pressure = reader.value_int();
         } else if (!strcmp("humidity", reader.value()) && reader.read()) {
           humidity = reader.value_int();
+        } else if (!strcmp("weather", reader.value())) {
+          state = J_CURRENT_WEATHER;
+        }
+      } else if (state == J_DAILY) {
+        if (!strcmp("temp", reader.value())) {
+          state = J_DAILY_TEMP;
+        }
+      } else if (state == J_DAILY_TEMP) {
+        if (!strcmp("max", reader.value()) && reader.read()) {
+          max_temp = reader.value_real();
+        } else if (!strcmp("min", reader.value()) && reader.read()) {
+          min_temp = reader.value_real();
         }
       }
     }
   }
 
   http_end(handle);
+}
 
+void draw_screen() {
   time_t now;
   struct tm current_time;
   time(&now);
 
+  // adjust for our timezone
   now += UTC_OFFSET + DST_OFFSET;
 
   localtime_r(&now, &current_time);
 
+  // offset sunrise and sunset times as they are given in UTC
   struct tm sunrise_time = unixToTm(sunrise + UTC_OFFSET + DST_OFFSET);
   struct tm sunset_time = unixToTm(sunset + UTC_OFFSET + DST_OFFSET);
-
-  uint8_t status_code = floor(weather_id / 100);
 
   auto sunrise_seconds = (sunrise_time.tm_hour * 60 * 60) +
                          (sunrise_time.tm_min * 60) + sunrise_time.tm_sec;
@@ -276,6 +270,8 @@ void fetch_data() {
                         (sunset_time.tm_min * 60) + sunset_time.tm_sec;
   auto current_seconds = (current_time.tm_hour * 60 * 60) +
                          (current_time.tm_min * 60) + current_time.tm_sec;
+
+  uint8_t status_code = floor(weather_id / 100);
 
   if (status_code == 6) {
     weather_icon.image(snowflake_icon);
@@ -307,43 +303,58 @@ void fetch_data() {
   }
 
   snprintf(temp_text, sizeof(temp_text), temp_format_string, temp);
-  snprintf(pres_text, sizeof(pres_text), pres_format_string, pressure);
-  snprintf(time_text, sizeof(time_text), time_format_string,
-           current_time.tm_hour, current_time.tm_min);
-  snprintf(hume_text, sizeof(hume_text), hume_format_string, humidity);
   snprintf(short_temp_text, sizeof(short_temp_text), short_temp_format_string,
            temp);
+  snprintf(temp_range_text, sizeof(temp_range_text), temp_range_format_string,
+           min_temp, max_temp);
+  snprintf(time_text, sizeof(time_text), time_format_string,
+           current_time.tm_hour, current_time.tm_min);
+  snprintf(long_time_text, sizeof(long_time_text), long_time_format_string,
+           current_time.tm_hour, current_time.tm_min);
 
-  time_label.text(time_text);
-  pres_label.text(pres_text);
   temp_label.text(temp_text);
-  hume_label.text(hume_text);
+  time_label.text(time_text);
+  temp_range_label.text(temp_range_text);
 
-  lcd.update();
+  // todo: update humidity progress bar
+  float humidityPercent = humidity / 100.0f;
+
+  while (lcd.dirty()) {
+    lcd.update();
+  }
 
   vTaskDelay(pdMS_TO_TICKS(5000));
-  // animate_time_label(&hide_label);
-  // time_label.text(short_temp_text);
-  // animate_time_label(&show_label_left);
-  // vTaskDelay(pdMS_TO_TICKS(5000));
-  // animate_time_label(&hide_label);
-  // time_label.text(time_text);
-  // animate_time_label(&show_label_right);
-  animate_small_label(&hide_temp);
-  animate_small_label(&hide_hume);
-  animate_small_label(&hide_pres);
+  animate_time_label(&hide_label);
+  temp_label.text(long_time_text);
+  time_label.text(short_temp_text);
+  animate_time_label(&show_label_left);
+  vTaskDelay(pdMS_TO_TICKS(5000));
+  animate_time_label(&hide_label);
+  temp_label.text(temp_text);
+  time_label.text(time_text);
+  animate_time_label(&show_label_right);
 }
 
 void app_loop(void* params) {
+  uint8_t counter = 4;
+
   while (true) {
     uint32_t started = pdTICKS_TO_MS(xTaskGetTickCount());
 
-    fetch_data();
+    if (++counter == 5) {
+      counter = 0;
+      fetch_data();
+    }
+
+    draw_screen();
 
     uint16_t elapsed = pdTICKS_TO_MS(xTaskGetTickCount()) - started;
 
     vTaskDelay(pdMS_TO_TICKS(60000 - elapsed));
   }
+}
+
+void test(rect_t::control_surface_type& dst, const srect16& clip, void* state) {
 }
 
 extern "C" void app_main() {
@@ -380,13 +391,13 @@ extern "C" void app_main() {
   esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(NTP_SERVER);
   ESP_ERROR_CHECK(esp_netif_sntp_init(&config));
 
-  int retry = 0;
-
   loading_label.text(sync_time_format_string);
   lcd.update();
 
+  int retry = 0;
+
   while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < 10) {
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    vTaskDelay(pdMS_TO_TICKS(500));
   }
 
   loading_label.text(fetching_format_string);
@@ -402,23 +413,22 @@ extern "C" void app_main() {
   time_label.text_justify(uix_justify::top_left);
   main_screen.register_control(time_label);
 
-  temp_label.bounds(srect16(0, 0, LCD_WIDTH - 32, 10));
+  temp_label.bounds(srect16(0, 0, LCD_WIDTH - 32, 16));
   temp_label.font(small_text_font);
   temp_label.color(uix_color_t::white);
   temp_label.text_justify(uix_justify::top_left);
   main_screen.register_control(temp_label);
 
-  hume_label.bounds(srect16(0, 10, LCD_WIDTH - 32, 20));
-  hume_label.font(small_text_font);
-  hume_label.color(uix_color_t::white);
-  hume_label.text_justify(uix_justify::top_left);
-  main_screen.register_control(hume_label);
+  temp_range_label.bounds(srect16(0, 16, LCD_WIDTH - 32, 32));
+  temp_range_label.font(small_text_font);
+  temp_range_label.color(uix_color_t::white);
+  temp_range_label.text_justify(uix_justify::top_left);
+  main_screen.register_control(temp_range_label);
 
-  pres_label.bounds(srect16(0, 20, LCD_WIDTH - 32, 30));
-  pres_label.font(small_text_font);
-  pres_label.color(uix_color_t::white);
-  pres_label.text_justify(uix_justify::top_left);
-  main_screen.register_control(pres_label);
+  hume_rect.bounds(srect16(0, 0, LCD_WIDTH - 33, 15));
+  hume_rect.on_paint_callback(test);
+  main_screen.register_control(hume_border);
+  main_screen.register_control(hume_rect);
 
   weather_icon.bounds(srect16(LCD_WIDTH - 32, 0, LCD_WIDTH, 32));
   weather_icon.image(question_icon);
